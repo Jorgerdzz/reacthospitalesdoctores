744ad0d270d8e0c30272acabc7af02ea8cbf7795
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-21 10:14:56 +0200

    Inicio proyecto

---
+++ src/components/Router.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default class Router extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+      </BrowserRouter>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MenuHospitales from './components/MenuHospitales';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MenuHospitales />
+    <Home />
   </React.StrictMode>
 );
 
